09277f115aa375f90a61d391f8bf0566ba3769c7
amooryjubran <omarsalehjubran@gmail.com>
2020-07-23 14:24:24 -0400

    fixes

--- src/pages/ChoosePlan.js
+++ src/pages/ChoosePlan.js
@@ -52,92 +52,90 @@ export default ChoosePlan;
 
 // the main container
 const MainContainer = styled.div`
-    background: #fff;
+  background: #fff;
 
-    // header top
-    .header-top {
+  // header top
+  .header-top {
     background: #fff;
     height: 6rem;
     border-bottom: 1px solid #e6e6e6;
+  }
+
+  // header content
+  .header-content {
+    display: grid;
+    justify-content: center;
+    background: #fff;
+    color: var(--main-dark);
+    margin-bottom: 6rem;
+    width: 65%;
+    position: relative;
+    margin: auto;
+    margin-top: 4.5rem;
+    text-align: center;
+    align-content: center;
+    flex-direction: column;
+    z-index: 2;
+  }
+
+  // checkmark logo
+  .checkmark-logo {
+    width: 3rem;
+    margin-top: 3.125rem;
+    margin-bottom: 2rem;
+  }
+
+  .header-content img {
+    margin: 6.25rem auto 1.875rem;
+  }
+
+  // the checklist
+  .checked-list {
+    text-align: left;
+    margin: 1rem auto 3rem;
+    padding-left: 1.5625rem;
+    width: 85%;
+    font-size: 17px;
+    padding: auto;
+  }
+
+  // bullets
+  .bullet {
+    margin-top: 1rem;
+    margin-left: 1rem;
+    text-indent: -1em;
+    line-height: 1.2rem;
+  }
+
+  .checked-list .bullet::before {
+    color: transparent;
+    display: inline-block;
+    position: relative;
+    height: 0.3em;
+    width: 0.8em;
+    content: "";
+    left: -0.9375rem;
+    bottom: 0.1875rem;
+    border-bottom: 1px solid #e50914;
+    border-left: 1px solid #e50914;
+    transform: rotate(-45deg);
+  }
+
+  // the signin btn
+  .signIn-btn {
+    margin: 1.5625rem 3% 0;
+    padding: 0.4375rem 1.0625rem;
+    font-weight: 700;
+    line-height: normal;
+    color: var(--main-dark);
+    font-size: 1.25rem;
+    right: 0;
+    position: absolute;
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
     }
-
-    // header content
-    .header-content {
-      display: grid;
-      justify-content: center;
-      background: #fff;
-      color: var(--main-dark);
-      margin-bottom: 6rem;
-      width: 65%;
-      position: relative;
-      margin: auto;
-      margin-top: 4.5rem;
-      text-align: center;
-      align-content: center;
-      flex-derection: column;
-      z-index: 2;
-    }
-
-    // checkmark logo
-    .checkmark-logo {
-        width: 3rem;
-        margin-top: 3.125rem;
-        margin-bottm: 2rem;
-    }
-
-    .header-content img {
-        margin: 6.25rem auto 1.875rem;
-    }
-
-    // the checklist
-    .checked-list {
-        text-align: left;
-        margin: 1rem auto 3rem;
-        padding-left: 1.5625rem;
-        width: 85%;
-        font-size: 17px;
-        padding: auto;
-    }
-
-    // bullets
-    .bullet {
-        margin-top: 1rem;
-        margin-left: 1rem;
-        text-indent: -1em;
-        line-height: 1.2rem;
-    }
-
-    .checked-list .bullet::before {
-        color: transparent;
-        display: inline-block;
-        position: relative;
-        height: 0.3em;
-        width: 0.8em;
-        content: '';
-        left: -0.9375rem;
-        bottom: 0.1875rem;
-        border-bottom: 1px solid #e50914;
-        border-left: 1px solid #e50914;
-        transform: rotate(-45deg);
-      }
-
-      // the signin btn
-    .signIn-btn {
-        margin: 1.5625rem 3% 0;
-        padding: 0.4375rem 1.0625rem
-        font-weight: 700;
-        line-height: normal;
-        color: var(--main-dark);
-        font-size: 1.25rem;
-        right: 0;
-        position: absolute;
-        cursor: pointer;
-        &:hover {
-        text-decoration: underline;
-        }
-    
-    }
-
+  }
 `;
 
 // the logo (netflix)
